fa18e3464c86047c82f4e54c961e8e9a9d645806
Jerry Pallath <jerrypallath@jerrys-mbp.lan>
2024-03-18 19:50:14 -0400

    updates to login

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,7 @@
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions, Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -41,29 +42,13 @@ export const options: NextAuthOptions = {
       return session;
     },
     async signIn({ profile }) {
-      console.log(profile);
+      const user = await prisma.user.findUnique({
+        where: { email: profile?.email },
+      });
+      if (!user) {
+        return false;
+      }
       return true;
     },
   },
-
-  //   {
-  //   iss: 'https://accounts.google.com',
-  //   azp: '7620310533-s2ke3gghbdthpb589k731m7qg6ib0a3q.apps.googleusercontent.com',
-  //   aud: '7620310533-s2ke3gghbdthpb589k731m7qg6ib0a3q.apps.googleusercontent.com',
-  //   sub: '110578764179294976909',
-  //   email: 'pallathj@gmail.com',
-  //   email_verified: true,
-  //   at_hash: 'uTFfwfoKIoTrtFZWNB7cmw',
-  //   name: 'Jerry Pallath',
-  //   picture: 'https://lh3.googleusercontent.com/a/ACg8ocLtFPVEfRicpDsKO2LYs4NUOoCsdh-ZuK7XwDfXSFZFEip_=s96-c',
-  //   given_name: 'Jerry',
-  //   family_name: 'Pallath',
-  //   locale: 'en',
-  //   iat: 1710451111,
-  //   exp: 1710454711
-  // }
-
-  // pages:{
-  //     signIn: "/signin"
-  // }
 };
