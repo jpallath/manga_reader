7672dfcba42f984c35dd9f38a84822997bd843ff
Jerry Pallath <jerrypallath@jerrys-mbp.lan>
2024-03-20 11:20:23 -0400

    remove credentials provider

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -10,33 +10,6 @@ export const options: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: {
-    //       label: "Username:",
-    //       type: "text",
-    //       placeholder: "username",
-    //     },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "******",
-    //     },
-    //   },
-    //   async authorize(credentials) {
-    //     const user = { id: "42", name: "Dave", password: "nextauth" };
-
-    //     if (
-    //       credentials?.username === user.name &&
-    //       credentials?.password === user.password
-    //     ) {
-    //       return user;
-    //     } else {
-    //       return null;
-    //     }
-    //   },
-    // }),
   ],
   callbacks: {
     async session({ session }) {
