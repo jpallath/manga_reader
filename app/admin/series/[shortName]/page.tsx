6ff3cbff8831cbd783de988956f7e00609191721
Jerry Pallath <jerrypallath@jerrys-mbp.lan>
2024-03-25 10:59:58 -0400

    more pages

---
+++ app/admin/series/[shortName]/page.tsx
@@ -0,0 +1,3 @@
+export default async function AdminSeriesIndividualPage() {
+  return <div>testing</div>;
+}
---
+++ app/admin/users/page.tsx
@@ -0,0 +1,3 @@
+export default async function AdminUsersPage() {
+  return <div>users</div>;
+}
